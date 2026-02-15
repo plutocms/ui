a151a90992d1e3abbb5290849914cdfe56541efb
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-15 16:46:20 -0300

    feat: created `PostTitleInput` component

---
+++ app/components/PostTitleInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  placeholder?: string
+}
+
+const props = defineProps<Props>()
+
+const model = defineModel<string>()
+</script>
+
+<template>
+  <div class="grow bg-transparent">
+    <input
+      v-model="model"
+      v-autofocus
+      :placeholder="props.placeholder || 'Add title'"
+      type="text"
+      class="light:text-zinc-600 h-full w-full px-8 pt-8 text-4xl font-bold outline-0 dark:text-white placeholder:text-zinc-500 focus:placeholder:text-zinc-400"
+    />
+  </div>
+</template>
