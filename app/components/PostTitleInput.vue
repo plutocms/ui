20e3543160ff2a74fbcef59406f767885c442d27
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-17 03:54:52 -0300

    style: enhance input styling and add icon for better UX in PostTitleInput component

--- app/components/PostTitleInput.vue
+++ app/components/PostTitleInput.vue
@@ -13,14 +13,19 @@ const model = defineModel<string>()
 </script>
 
 <template>
-  <div class="grow bg-transparent">
+  <div class="group relative grow bg-transparent">
     <input
       v-model="model"
       v-bind="$attrs"
       v-autofocus
       :placeholder="props.placeholder || 'Add title'"
       type="text"
-      class="light:text-zinc-600 h-full w-full px-8 pt-8 text-4xl font-bold outline-0 dark:text-white placeholder:text-zinc-500 focus:placeholder:text-zinc-400"
+      class="light:text-zinc-600 cursor-pointer focus:cursor-text h-full w-full px-8 pt-8 text-4xl font-bold outline-0 dark:text-zinc-400 dark:focus:text-white placeholder:text-zinc-500 focus:placeholder:text-zinc-400"
+    />
+
+    <Icon
+      name="lucide:edit"
+      class="absolute right-4 top-1/2 text-2xl pointer-events-none opacity-100 group-focus-within:opacity-0"
     />
   </div>
 </template>
