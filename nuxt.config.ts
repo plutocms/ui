60d5fe019efe0a49b6bf18e63fd2d00620fc06c6
Victor Ribeiro <devjvribeiro@gmail.com>
2026-03-02 02:27:59 -0300

    chore: update dependencies and trustedDependencies in package.json and bun.lock; fix extends path in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['github:plutocms/utils'],
+  extends: ['@plutocms/utils'],
 
   modules: ['@nuxt/ui'],
 
